0b4239b8d790e1fa9c628192188a3e105c92f555
Fiju <faixanshirax@gmail.com>
2021-08-18 22:23:24 +0500

    Another try

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,12 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
-
+console.log(config);
 export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
   host: config.host,
-  port: 5432,
   dialect: "postgres",
   storage: ":memory:",
 });
