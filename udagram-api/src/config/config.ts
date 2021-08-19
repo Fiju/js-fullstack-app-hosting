f2da4e2fdae004bb39d3f4c3175fb251cbc5697b
Fiju <faixanshirax@gmail.com>
2021-08-19 23:39:53 +0500

    Removed sequelize static string

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
+console.log(process.env);
+
 export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,17 +1,11 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 console.log(config);
-export const sequelize = new Sequelize(
-  "postgres://postgres:postgres@udagram-db.cc4sjlh5deyt.us-east-1.rds.amazonaws.com:5432/postgres"
-);
-// ({
-//   username: config.username,
-//   password: config.password,
-//   database: config.database,
-//   host: config.host,
-//   dialect: "postgres",
-//   storage: ":memory:",
-// });
-// (
-//   "postgres://postgres:postgres@udagram-db.cc4sjlh5deyt.us-east-1.rds.amazonaws.com:5432/postgres"
-// );
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  dialect: "postgres",
+  storage: ":memory:",
+});
